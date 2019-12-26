24b8e299a783fa3e2edd9f4d876904be1c382a3f
ktscates <cates2506@gmail.com>
2019-12-26 19:02:00 +0200

    First commit

---
+++ src/main/java/App.java
@@ -0,0 +1,52 @@
+import models.Stock;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Map<String, Object> model = new HashMap<String, Object>();
+        String layout = "templates/layout.hbs";
+
+        get("/", (request, response) -> {
+            model.put("stock", Stock.all());
+            return new ModelAndView(model, "stock.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/stock", (request, response) -> {
+            model.put("stock", Stock.all());
+            return new ModelAndView(model, "stock.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/stock", (request, response) -> {
+            String name= request.queryParams("name");
+            int quantity = Integer.parseInt(request.queryParams("quantity"));
+            int priceUnit = Integer.parseInt(request.queryParams("priceUnit"));
+            int totalPrice = quantity * priceUnit;
+            Stock newStock = new Stock(name, quantity, priceUnit, totalPrice);
+            model.put("name", newStock.getName());
+            model.put("quantity", newStock.getQuantity());
+            model.put("priceUnit", newStock.getPriceUnit());
+            model.put("totalPrice", newStock.getTotalPrice());
+            newStock.save();
+            model.put("stock", Stock.all());
+            model.put("StockClass", Stock.class);
+            return new ModelAndView(model, "stock.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/delete/:id", (req, res) -> {
+            int delete = Integer.parseInt(req.params("id"));
+            Stock.deleteById(delete);
+            res.redirect("/stock");
+            return null;
+        }, new HandlebarsTemplateEngine());
+    }
+}
---
+++ src/main/java/models/Stock.java
@@ -0,0 +1,99 @@
+package models;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Stock {
+
+    private int id;
+    private String date;
+    private String name;
+    private int quantity;
+    private int priceUnit;
+    int totalPrice = 0;
+
+    public Stock(String name, int quantity, int priceUnit, int totalPrice){
+        this.name = name;
+        this.quantity = quantity;
+        this.priceUnit = priceUnit;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPriceUnit() {
+        return priceUnit;
+    }
+
+    public void setPriceUnit(int priceUnit) {
+        this.priceUnit = priceUnit;
+    }
+
+    public int getTotalPrice(){
+        totalPrice = quantity * priceUnit;
+        return totalPrice;
+    }
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO stock(date, name, quantity, priceunit, totalprice) VALUES(now(), :name, :quantity, :priceUnit, :totalPrice);";
+            con.createQuery(sql)
+                    .addParameter("name", this.name)
+                    .addParameter("quantity", this.quantity)
+                    .addParameter("priceUnit", this.priceUnit)
+                    .addParameter("totalPrice", this.totalPrice)
+                    .executeUpdate();
+        }
+    }
+
+    public static List<Stock> all(){
+        String sql = "SELECT * FROM stock";
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(Stock.class);
+        }
+    }
+
+    public static void deleteById(int id) {
+        String sql = "DELETE from stock WHERE id=:id";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+}
