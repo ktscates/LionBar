daf661939d21615e334adface4cab1edcd0068e4
ktscates <cates2506@gmail.com>
2020-01-08 13:46:24 +0200

    Add files for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/lionBar/bin/lionBar
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,13 +8,23 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
+import static spark.Spark.*;
 
 
 public class App {
 
     public static void main(String[] args) {
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
 
         Map<String, Object> model = new HashMap<String, Object>();
         String layout = "templates/layout.hbs";
